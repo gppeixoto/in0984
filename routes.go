b360b7d19ac7baba96e26eebb7f8474b996ced70
Guilherme Peixoto <gpp@cin.ufpe.br>
2018-05-16 23:49:06 -0300

    feat(routes.go): implements initial match logic w/ twitter trends

--- routes.go
+++ routes.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/labstack/echo"
+)
+
+var (
+	matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+	matchAllCap   = regexp.MustCompile("([a-z0-9])([A-Z])")
+	fakeReq       = "{\"text\": \"data\"}"
 )
 
 type Server interface {
@@ -13,20 +21,42 @@ type Server interface {
 }
 
 type server struct {
-	server *echo.Echo
-}
-
-type TextData struct {
-	Text string `json:"text"`
+	server  *echo.Echo
+	twitter TwitterTrendsSvc
 }
 
 func (s *server) analyzerHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
+		// get data request
 		t := new(TextData)
 		if err := c.Bind(t); err != nil {
-			return err
+			return c.JSON(http.StatusBadRequest,
+				fmt.Sprintf("request should look like %s", fakeReq),
+			)
 		}
-		return c.JSON(http.StatusOK, t)
+		textTokens := getTextTokens(t.Text)
+
+		// prepare
+		trends, err := s.twitter.Trends()
+		if err != nil {
+			echo.NewHTTPError(http.StatusInternalServerError, "unable to reach twitter")
+		}
+		for _, trend := range trends {
+			topicTokens := getTopicTokens(trend.Name)
+			if matchedToken, ok := match(textTokens, topicTokens); ok {
+				res := &Response{
+					Text: fmt.Sprintf("match found %+v matching with \"%v\"", trend, matchedToken),
+				}
+				return c.JSON(
+					http.StatusOK,
+					res,
+				)
+			}
+		}
+		return echo.NewHTTPError(
+			http.StatusNotFound,
+			fmt.Sprintf("did not find a match for %v", t.Text),
+		)
 	}
 }
 
@@ -35,6 +65,7 @@ func (s *server) registerRoutes() {
 }
 
 func (s *server) Close() {
+	s.twitter.Close()
 	s.server.Close()
 }
 
@@ -45,6 +76,43 @@ func (s *server) Start(port int) {
 
 func NewServer() Server {
 	return &server{
-		server: echo.New(),
+		server:  echo.New(),
+		twitter: NewTwitterTrendsSvc(23424768), // Brazil WOEID
 	}
+}
+
+func getTopicTokens(str string) (tokens map[string]bool) {
+	tokens = make(map[string]bool)
+	var ss string
+	if str[0] == '#' {
+		ss = str[1:len(str)]
+	}
+	ss = matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	ss = matchAllCap.ReplaceAllString(ss, "${1}_${2}")
+	ss = strings.ToLower(ss)
+
+	for _, token := range strings.Split(ss, "_") {
+		tokens[token] = true
+	}
+
+	return
+}
+
+func getTextTokens(str string) (tokens map[string]bool) {
+	tokens = make(map[string]bool)
+	for _, token := range strings.Split(str, " ") { // TODO: use proper tokenization
+		if len(token) > 2 {
+			tokens[token] = true // instead of splitting on whitespace
+		}
+	}
+	return
+}
+
+func match(one, other map[string]bool) (string, bool) {
+	for element := range one {
+		if other[element] {
+			return element, true
+		}
+	}
+	return "", false
 }
