cd35549b015d2a0533dfea3da6668e8e48a5567f
Guilherme Peixoto <gpp@cin.ufpe.br>
2018-06-14 00:19:32 -0300

    feat(models.go): adds sample_size parameter on request and tweets samples on response

--- models.go
+++ models.go
@@ -1,17 +1,21 @@
 package main
 
+// TextData defines model for api request
 type TextData struct {
-	Text string `json:"text"`
+	Text             string `json:"text"`
+	TweetsSampleSize int    `json:"tweets_sample_size"`
 }
 
+// Response defines API's response
 type Response struct {
-	Name        string  `json:"name"`
-	Score       float64 `json:"sentiment_score"`
-	Magnitude   float64 `json:"sentiment_strength"`
-	TweetVolume int64   `json:"tweet_volume"`
+	Name         string   `json:"name"`
+	Score        float64  `json:"sentiment_score"`
+	Magnitude    float64  `json:"sentiment_strength"`
+	TweetVolume  int64    `json:"tweet_volume"`
+	SampleTweets []string `json:"sample_tweets"`
 }
 
-type Match struct {
+type trendingTopicMatch struct {
 	query  string
 	name   string
 	volume int64
